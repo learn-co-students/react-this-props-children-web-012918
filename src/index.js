8d1d0a8162300654725b0153ef7606c1e6b6458f
scott <you@example.comitsmescottb123@gmail.com>
2018-04-09 16:41:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// Code Goes Here
+import React from 'react'
+
+
+export default class Panel extends React.Component {
+  render() {
+    return (
+      <div className="panel">
+        <div className="panel-header">{this.props.title}</div>
+        <div className="panel-body">{this.props.children}</div>
+      </div>
+    );
+  }
+}
